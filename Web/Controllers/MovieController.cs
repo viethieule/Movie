69c550fab7e965b58f0e485f04f3ec8d6d4f80d5
Hieu Le <Le.Hieu@steadfasttech.com.au>
2022-01-05 00:13:23 +0700

    torrent search & download poc

--- Web/Controllers/MovieController.cs
+++ Web/Controllers/MovieController.cs
@@ -1,16 +1,89 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web;
+using Web.Models;
 
 namespace Web.Controllers
 {
     public class MovieController : Controller
     {
+        private static HttpClient _httpClient = new HttpClient();
         public IActionResult Index(string name)
         {
             ViewBag.Name = name;
+            return View();
+        }
+
+        public async Task<IActionResult> Search(string searchTerm)
+        {
+            ViewBag.Query = searchTerm;
+
+            string response = await _httpClient.GetStringAsync("https://yts.mx/api/v2/list_movies.json?query_term=" + HttpUtility.UrlEncode(searchTerm));
+            var definition = new
+            {
+                Data = new
+                {
+                    Movies = new[]
+                    {
+                        new
+                        {
+                            Id = 0,
+                            Title = string.Empty,
+                            TitleLong = string.Empty,
+                            SmallCoverImage = string.Empty,
+                            MediumCoverImage = string.Empty,
+                            Torrents = new[]
+                            {
+                                new
+                                {
+                                    Hash = string.Empty,
+                                    Quality = string.Empty
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            JsonSerializerSettings serializerSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new DefaultContractResolver
+                {
+                    NamingStrategy = new SnakeCaseNamingStrategy()
+                }
+            };
+
+            var typedResponse = JsonConvert.DeserializeAnonymousType(response, definition, serializerSettings);
+            List<MovieSearchViewModel> movies = typedResponse.Data.Movies.Select(movie => new MovieSearchViewModel
+            {
+                Id = movie.Id,
+                Title = movie.Title,
+                TitleLong = movie.TitleLong,
+                ImageMediumUrl = movie.MediumCoverImage,
+                ImageSmallUrl = movie.SmallCoverImage,
+                Hash = movie.Torrents.First(x => x.Quality == "1080p").Hash
+            }).ToList();
+
+            return View(movies);
+        }
+
+        public async Task<IActionResult> Download(string hash, string title)
+        {
+            string tracker = "udp://p4p.arenabg.com:1337";
+            string magnet = $"magnet:?xt=urn:btih:{hash}&dn={HttpUtility.UrlEncode(title)}&tr={tracker}";
+            Process process = new Process();
+            process.StartInfo = new ProcessStartInfo(magnet)
+            {
+                UseShellExecute = true
+            };
+            process.Start();
             return View();
         }
     }
---
+++ Web/Models/MovieSearchViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+    public class MovieSearchViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string TitleLong { get; set; }
+        public string ImageSmallUrl { get; set; }
+        public string ImageMediumUrl { get; set; }
+        public string Hash { get; set; }
+    }
+}
